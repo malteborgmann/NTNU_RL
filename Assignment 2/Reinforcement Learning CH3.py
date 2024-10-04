19c857acce08d33726b2670e73f43f9da341b606
Malte Borgmann <malte@borgmann.tech>
2024-10-04 21:35:41 +0800

    Comment on back action

--- Assignment 2/Reinforcement Learning CH3.py
+++ Assignment 2/Reinforcement Learning CH3.py
@@ -66,3 +66,27 @@ for i in range(states):
                 policy[i] = 'T'
 
 print("The algoirthm's final policy is:", policy)
+
+
+# Back-Action
+"""
+The model for a back-action could be done in four different approaches:
+1. Negative Reward
+     - This model would lead to a penalty for taking the back-action.
+     - This way we would prevent the agent from taking the back-action to often.
+     - This could lead to a loop when previous V_pi is higher than the negative reward of the back_action
+2. No Reward -> Reward of 0
+     - This model would lead to the agent taking back-action for a kind of infinite reward trick.
+     - For example when V_pi is negative
+     - But this could lead the agent to be stuck in a loop. Switching from s2 to s5 and back would be a reward loop, because the reward for every second action is +3
+3. Positive Reward
+     - This model would lead to the agent taking the back-action more often.
+     - We would get the same problem as in the No Reward -> Reward of 0 model.
+     - Switching between two states endless would lead to a reward loop.
+4. Negative Reward that is as high as the previous reward
+     - This model would lead to the agent taking the back-action only when it is necessary.
+     - The agent would only take the back-action when V_pi is lower than the previous V_pi.
+
+In conclusion the best approach would be the fourth model. 
+It must be noted that if the number of iterations is high enough the final policy would be pretty much the same as without a back-action.
+"""
